99ab1e4cb61d5352dc4c541f69161cf57ad779d0
Brian Kim <brian2562@gmail.com>
2024-09-10 23:39:48 +0900

    add profile picture

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,23 @@ import './App.css';
 
 import { useState } from 'react';
 
-import viteLogo from '/vite.svg';
-
-import reactLogo from './assets/react.svg';
+import brianpfp from './assets/brian_githubpfp.jpg';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <div className="pfp">
+        <a
+          href="https://github.com/GlassyFoozle"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={brianpfp} className="logo react" alt="React logo" />
         </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>김채민</h1>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button
           onClick={() => {
