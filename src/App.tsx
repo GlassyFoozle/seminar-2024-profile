4d780d90da42d2a9b55e09aee5ab0166d593e568
Brian Kim <brian2562@gmail.com>
2024-09-11 18:34:27 +0900

    overall finishing touches

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import './App.css';
 
+import { useState } from 'react';
+
 import brianpfp from './assets/brian_githubpfp.jpg';
 
 function App() {
+  const [hidden, setHidden] = useState(true);
+
   return (
-    <>
-      <div className="profile">
-        <p className="hide">고양이 클릭해서 GitHub 가보기!</p>
+    <div className="box">
+      <div
+        className="profile"
+        onMouseEnter={() => {
+          setHidden(false);
+        }}
+        onMouseLeave={() => {
+          setHidden(true);
+        }}
+      >
+        {hidden ? null : <p>고양이 클릭해서 GitHub 가보기!</p>}
         <a
           href="https://github.com/GlassyFoozle"
           target="_blank"
@@ -14,9 +26,8 @@ function App() {
         >
           <img src={brianpfp} className="pfp" alt="profile image" />
         </a>
-
-        <h1>김채민</h1>
       </div>
+      <h1>김채민</h1>
       <div className="details">
         <ul>
           <li>조선해양공학과 20학번 01년생</li>
@@ -25,7 +36,7 @@ function App() {
           <li>MBTI는 ISFJ입니다!</li>
         </ul>
       </div>
-    </>
+    </div>
   );
 }
 
